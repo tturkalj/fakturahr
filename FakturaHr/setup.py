575fdcfde7ab6625ea11f2cf28c6916d0056ea11
tturkalj <tturka33@gmail.com>
2017-04-16 18:37:33 +0200

    updated requirements and readme

--- FakturaHr/setup.py
+++ FakturaHr/setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
-from FakturaHr import __version__
+
+
+__version__ = 0.1
 
 setup(name='FakturaHr',
       version=__version__,
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fakturahr
+
+Manager your clients, items and receipts.
+
+Install guide:
+
+
+pip install -r FakturaHr/requirements.txt
+
+python setup.py develop
